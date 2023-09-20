80a37760cf853e2d8cb6dce79336bb718255e881
HI-SAYA <qnfnsk45@naver.com>
2023-09-21 08:33:51 +0900

    0921

---
+++ src/main/java/com/ohgiraffers/comprehensive/Chap01ComprehensiveLectureSourceApplication.java
@@ -0,0 +1,13 @@
+package com.ohgiraffers.comprehensive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Chap01ComprehensiveLectureSourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Chap01ComprehensiveLectureSourceApplication.class, args);
+	}
+
+}
